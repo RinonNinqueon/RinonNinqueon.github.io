e3d83131ba0640eef5cc2171678150ba1fcd278f
Rinon Ninqueon <stepanbelov@gmail.com>
2015-06-27 23:17:09 +0600

    popup
    так

--- js/popup.js
+++ js/popup.js
@@ -4,7 +4,7 @@ function()
 	$('li img').on('click',function(){
 	var src = $(this).attr('alt');
     var img = '<img src="' + src + '" class="img-responsive"/>';
-    $('#myModal').modal('hide');
+    $('#myModal').modal();
     $('#myModal').on('shown.bs.modal',
 		function()
 		{
