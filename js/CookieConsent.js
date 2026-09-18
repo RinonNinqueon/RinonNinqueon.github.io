f3c35e24db14c38dc1e7c519f8932d21f98aca0e
Rinon Ninqueon <stepanbelov@gmail.com>
2026-09-18 13:51:36 +0700

    Метрика

--- js/CookieConsent.js
+++ js/CookieConsent.js
@@ -1,11 +1,20 @@
-if (!Cookies.get('agreement'))
-	setTimeout(showCookieConsent, 1000);
-else if (Cookies.get('agreement') == '1')
-	initCounter();
+(function(m,e,t,r,i,k,a){
+	m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+	m[i].l=1*new Date();
+	for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+	k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+})(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+document.addEventListener("DOMContentLoaded", () => {
+    
+    if (!Cookies.get('agreement')) {
+        setTimeout(showCookieConsent, 1000);
+    } else if (Cookies.get('agreement') == '1') {
+        initCounter();
+    }
+    
+});
 
 function showCookieConsent () {
 	const consent = document.getElementById('agreementConsent');
@@ -23,21 +32,17 @@ function hideMessage() {
 function saveYesAnswer()
 {
 	hideMessage();
-	Cookies.set('agreement', '1');
+	Cookies.set('agreement', '1', { expires: 365 });
+	saveYesAnswer();
 }
 
 function saveNoAnswer()
 {
 	hideMessage();
-	Cookies.set('agreement', '0');
+	Cookies.set('agreement', '0', { expires: 365 });
 }
 
-function initCounter ()
+function initCounter()
 {
-	ym(52735531, "init", {
-		clickmap:true,
-		trackLinks:true,
-		accurateTrackBounce:true
-	});
-	saveYesAnswer();
+	ym(52735531, 'init', {clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
 }
